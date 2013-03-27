43c84e544f3f2cfcc886728b4c9785a7dc315cc0
Yann Crumeyrolle <murk@club-internet.fr>
2013-03-28 00:50:39 +0100

    Add a transactionnal filter.

---
+++ CommandProcessing.Tests/Filters/TransactionAttributeFixture.cs
@@ -0,0 +1,77 @@
+﻿namespace CommandProcessing.Tests.Filters
+{
+    using System;
+    using System.Threading;
+    using System.Transactions;
+    using CommandProcessing.Filters;
+    using CommandProcessing.Tests.Helpers;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TransactionttributeFixture
+    {
+        [TestMethod]
+        public void WhenCreatingInstanceThenPropertiesAreDefined()
+        {
+            // Arrange & Act
+            TransactionFilter filter = new TransactionFilter();
+
+            // Assert
+            Assert.AreEqual(TransactionScopeOption.Required, filter.ScopeOption);
+            Assert.AreEqual(TransactionManager.DefaultTimeout, filter.Timeout);
+            Assert.AreEqual(IsolationLevel.Serializable, filter.IsolationLevel);
+        }
+
+        [TestMethod]
+        public void WhenInvokingFilterWithoutExceptionThenTransactionCompletes()
+        {
+            // Arrange
+            TransactionFilter filter = new TransactionFilter();
+            HandlerExecutingContext executingContext = new HandlerExecutingContext(new HandlerContext());
+            HandlerExecutedContext executedContext = new HandlerExecutedContext(executingContext, false, null);
+
+            // Act
+            filter.OnCommandExecuting(executingContext);
+            Transaction transaction = Transaction.Current.Clone();
+            filter.OnCommandExecuted(executedContext);
+
+            // Assert 
+            Assert.AreEqual(TransactionStatus.Committed, transaction.TransactionInformation.Status);
+        }
+
+        [TestMethod]
+        public void WhenInvokingFilterWithExceptionThenTransactionRollbacks()
+        {
+            // Arrange
+            TransactionFilter filter = new TransactionFilter();
+            HandlerExecutingContext executingContext = new HandlerExecutingContext(new HandlerContext());
+            HandlerExecutedContext executedContext = new HandlerExecutedContext(executingContext, false, new Exception());
+
+            // Act
+            filter.OnCommandExecuting(executingContext);
+            Transaction transaction = Transaction.Current.Clone();
+            filter.OnCommandExecuted(executedContext);
+
+            // Assert
+            Assert.AreEqual(TransactionStatus.Aborted, transaction.TransactionInformation.Status);
+        }
+        
+        [TestMethod]
+        public void WhenInvokingFilterWithHandledExceptionThenTransactionCompletes()
+        {
+            // Arrange
+            TransactionFilter filter = new TransactionFilter();
+            HandlerExecutingContext executingContext = new HandlerExecutingContext(new HandlerContext());
+            HandlerExecutedContext executedContext = new HandlerExecutedContext(executingContext, false, new Exception());
+            executedContext.ExceptionHandled = true;
+
+            // Act
+            filter.OnCommandExecuting(executingContext);
+            Transaction transaction = Transaction.Current.Clone(); 
+            filter.OnCommandExecuted(executedContext);
+
+            // Assert 
+            Assert.AreEqual(TransactionStatus.Committed, transaction.TransactionInformation.Status);
+        }
+    }
+}
---
+++ CommandProcessing/Filters/TransactionFilter.cs
@@ -0,0 +1,67 @@
+﻿namespace CommandProcessing.Filters
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Transactions;
+
+    /// <summary>
+    /// Represents a filter for encapsulate a transaction on handlers.
+    /// </summary>
+    public class TransactionFilter : FilterAttribute, IHandlerFilter
+    {
+        private const string Key = "__TransactionFilterKey";
+
+        public TransactionFilter()
+        {
+            this.ScopeOption = TransactionScopeOption.Required;
+            this.Timeout = TransactionManager.DefaultTimeout;
+            this.IsolationLevel = IsolationLevel.Serializable;
+        }
+
+        public TransactionScopeOption ScopeOption { get; set; }
+
+        public TimeSpan Timeout { get; set; }
+
+        public IsolationLevel IsolationLevel { get; set; }
+
+        public void OnCommandExecuting(HandlerExecutingContext context)
+        {
+            var stack = GetStack(context.CommandContext);
+            if (stack == null)
+            {
+                stack = new Stack<TransactionScope>();
+                context.CommandContext.Items[Key] = stack;
+            }
+
+            var options = new TransactionOptions { Timeout = this.Timeout, IsolationLevel = this.IsolationLevel };
+            var transactionScope = new TransactionScope(this.ScopeOption, options);
+            stack.Push(transactionScope);
+        }
+
+        public void OnCommandExecuted(HandlerExecutedContext context)
+        {
+            var stack = GetStack(context.Context.CommandContext);
+            if (stack != null && stack.Count > 0)
+            {
+                using (var scope = stack.Pop())
+                {
+                    if (null != scope && (context.Exception == null || context.ExceptionHandled))
+                    {
+                        scope.Complete();
+                    }
+                }
+            }
+        }
+
+        private static Stack<TransactionScope> GetStack(HandlerContext context)
+        {
+            object value;
+            if (context.Items.TryGetValue(Key, out value))
+            {
+                return value as Stack<TransactionScope>;    
+            }
+            
+            return null;
+        }
+    }
+}
