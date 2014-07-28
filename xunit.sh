533990d6d522022c282d4ae30010e6892db65dd7
Yann Crumeyrolle <murk@club-internet.fr>
2014-07-28 21:54:20 +0200

    add xunit files

--- xunit.sh
+++ xunit.sh
@@ -2,12 +2,11 @@
 
 mono --runtime=v4.0 .nuget/NuGet.exe install xunit.runners -Version 2.0.0-beta-build2700 -o packages
 mono --runtime=v4.0 .nuget/NuGet.exe install xunit.core -Version 2.0.0-beta-build2700 -o packages
-mkdir build
+
 cp packages/xunit.runners.2.0.0-beta-build2700/tools/* ./build
 cp packages/xunit.core.2.0.0-beta-build2700/lib/portable-net45+win+wpa81+wp80+monotouch+monoandroid/* ./build
 
 runTest(){
-
    cp $1* ./build
    
    mono --runtime=v4.0 ./build/xunit.console.exe ./build/$2
@@ -17,8 +16,6 @@ runTest(){
    fi
 }
 
-runTest test/FeatureFlipper.Tests/bin/Release/ FeatureFlipper.Tests.dll
-runTest test/FeatureFlipper.Unity.Tests/bin/Release/ FeatureFlipper.Unity.Tests.dll
-runTest test/FeatureFlipper.WebApi.Tests/bin/Release/ FeatureFlipper.WebApi.Tests.dll
+runTest Waffle.xunit
 
 exit 
